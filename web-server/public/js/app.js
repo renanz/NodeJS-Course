dd23b13007457280eddf56a4c7f6215e91e5914f
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-22 16:53:36 -0600

    fetching data from form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,8 +1,15 @@
 console.log("Client side js");
 
-fetch("http://localhost:3000/weather?address=Boston").then(res => {
-  res.json().then(data => {
-    if (data.error) console.log(data.error);
-    else console.log(data);
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log(search.value);
+  fetch(`http://localhost:3000/weather?address=${search.value}`).then(res => {
+    res.json().then(data => {
+      if (data.error) console.log(data.error);
+      else console.log(data);
+    });
   });
 });
