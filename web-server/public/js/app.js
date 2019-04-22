3aae1f49682b7999f6c5e8e17ac6fafc520291ce
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-22 16:43:52 -0600

    started fethcing info

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,8 @@
 console.log("Client side js");
+
+fetch("http://localhost:3000/weather?address=Boston").then(res => {
+  res.json().then(data => {
+    if (data.error) console.log(data.error);
+    else console.log(data);
+  });
+});
