f06a6cb8139a4cba45beaa87acac07c5a3d04dda
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-22 14:04:11 -0600

    created about and help html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,9 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello express");
-});
-
-app.get("/help", (req, res) => {
-  res.send("Help");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About<h1>");
-});
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/weather", (req, res) => {
   res.send({ location: "Some Place", forecast: 22 });
