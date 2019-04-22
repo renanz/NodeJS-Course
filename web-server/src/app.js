0891215a37d7238c50bbe0b7dd869dff6a86dce1
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-22 14:43:51 -0600

    added hbs routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,15 +3,24 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("", (req, res) => {
+  res.render("index", { title: "Weather App" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About", name: "Renan Zelaya" });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", { title: "Help", message: "Some help message" });
+});
 
 app.get("/weather", (req, res) => {
   res.send({ location: "Some Place", forecast: 22 });
 });
-
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
