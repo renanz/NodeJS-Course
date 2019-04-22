30e483779cf4b4d7f2060595c529c8e0b054a1b4
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-22 10:53:43 -0600

    implementing callbacks for response

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,41 +3,30 @@ const request = require("request");
 
 const URI = {};
 
-URI.darksky = `https://api.darksky.net/forecast/${
-  secret.darksky
-}/37.8267, -122.4233?lang=es&units=si`;
+const geocode = (address, callback) => {
+  URI.mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${secret.mapbox}&limit=1`;
 
-request({ url: URI.darksky, json: true }, (err, res) => {
-  if (err) {
-    console.log("Unable to connect to weather service");
-  } else if (res.body.error) {
-    console.log("Unable to find location");
-  } else {
-    const { currently, daily } = res.body;
-    console.log(
-      `${daily.data[0].summary} Currently is ${
-        currently.temperature
-      } degrees out. There is a ${currently.precipProbability}% chance of rain`
-    );
-  }
+  request({ url: URI.mapbox, json: true }, (err, res) => {
+    if (err) callback("Unable to connect to location services!", undefined);
+    else if (res.body.features.length === 0)
+      callback("Unable to find location. Try another search", undefined);
+    else {
+      const { features } = res.body;
+      callback(undefined, {
+        latitude: features[0].center[1],
+        longitude: features[0].center[1],
+        place_name: features[0].place_name
+      });
+    }
+  });
+};
+
+geocode("Philadelphia", (err, data) => {
+  console.log(data);
 });
 
-// Geocoding
-// Address -> Lat/Long -> Weather
-
-URI.mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${
-  secret.mapbox
-}&limit=1`;
-
-request({ url: URI.mapbox, json: true }, (err, res) => {
-  if (err) {
-    console.log("Unable to access location service");
-  } else if (res.body.features.length === 0) {
-    console.log("Unable to find location. Try another search");
-  } else {
-    const { features } = res.body;
-    const latitude = features[0].center[1];
-    const longitude = features[0].center[0];
-    console.log(`${latitude}°, ${longitude}°`);
-  }
+geocode("New York", (err, data) => {
+  console.log(data);
 });
